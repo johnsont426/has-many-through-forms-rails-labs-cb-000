4ffcdad18b9fcd4c5b117cdbc042134d527ff9b3
Johnson Tai <johnsont426@gmail.com>
2017-04-14 04:06:11 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,19 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def post_id=(post_id)
+    self.post = Post.find_by(id: post_id)
+  end
+
+  def user_attributes=(user_attributes)
+    if !user_attributes.has_value?("")
+      self.user = User.find_or_create_by(username: user_attributes[:username])
+    end
+  end
+
+  def user_id=(user_id)
+    if user_id != ""
+      self.user = User.find(user_id)
+    end
+  end
 end
